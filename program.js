750c9daf84bbaa626b07219082d9fa7c9a096313
BBernYY <66414852+BBernYY@users.noreply.github.com>
2024-06-20 19:23:31 +0200

    init

--- program.js
+++ program.js
@@ -1,3 +1,8 @@
-function test(){
-    alert('true');
-}
+var audio = new Audio('watasigma.mp3');
+document.getElementsByTagName("svg")[0].addEventListener("mousedown", function(){
+    audio.play();
+})
+document.getElementsByTagName("svg")[0].addEventListener("mouseup", function(){
+    audio.pause();
+    audio.currentTime = 0;
+})
